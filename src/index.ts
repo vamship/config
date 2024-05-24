759e71686b0d2460b2b3271fa2662f02e5700dde
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2024-05-24 15:05:10 -0400

    Rename/delete files in preparation for rebuild

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-'use strict';
-
 /**
  * Utilites for initializing and accessing namespace scoped application
  * configuration.
  */
-module.exports = require('./config');
+export * from './config';
